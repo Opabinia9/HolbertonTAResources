9808eca26aac918f3f07024719ac6c03f19d875f
bfrasholb <13065@holbertonstudents.com>
2026-07-28 17:29:50 +1000

    Class struct

--- main.h
+++ main.h
@@ -1,12 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+typedef struct class_s {
+	char *name;
+	float multiplier;
+} class_t;
+
 typedef struct player_s {
-	char* name;
+	char *name;
 	unsigned int level;
 	unsigned int hp;
-	void (*repr)(struct player_s* p);
-	unsigned int (*attack)(struct player_s* p1, struct player_s* p2);
+	class_t *class;
+	void (*repr)(struct player_s *p);
+	unsigned int (*attack)(struct player_s *p1, struct player_s *p2);
 } player_t;
 
 #endif
